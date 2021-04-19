766e5bb26d8d459115d77382af1386421fc65dcf
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-19 17:55:42 -0300

    Employ Program class

--- Composition/Entities/Comment.cs
+++ Composition/Entities/Comment.cs
@@ -6,5 +6,15 @@ namespace Composition.Entities
 {
     class Comment
     {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
     }
 }
--- Composition/Program.cs
+++ Composition/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Composition.Entities;
+using System;
 
 namespace Composition
 {
@@ -6,7 +7,22 @@ namespace Composition
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+            DateTime d1 = DateTime.Parse("21/06/2018 13:05:44");
+            Post p1 = new Post(d1, "Traveling to New Zealand", "I'm going to visit this wonderful country!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+            DateTime d2 = DateTime.Parse("28/07/2018 23:14:19");
+            Post p2 = new Post(d2, "Good night guys", "See you tomorrow", 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
         }
     }
 }
